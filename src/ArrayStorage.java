ec08c8dca33058bd7b5e8aa7debc11c4e37944cd
Eugene <urbanj@yandex.ru>
2021-06-11 17:12:38 +0500

    temp

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,62 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = size(); i >= 0; --i) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        for (Resume resume : storage) {
+            if (resume != null && resume.uuid.equals(uuid)) {
+                return resume;
+            }
+        }
+        return new Resume();
     }
 
     void delete(String uuid) {
+        for (Resume resume : storage) {
+            if (resume.uuid != null && resume.uuid.equals(uuid)) {
+                resume.uuid = null;
+                bubbleSorter();
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
+        return storage.length;
+    }
+
+    private void toSwap(int first, int second){ //метод меняет местами пару чисел массива
+        Resume temp = storage[first];      //во временную переменную помещаем первый элемент
+        storage[first] = storage[second];       //на место первого ставим второй элемент
+        storage[second] = temp;          //вместо второго элемента пишем первый из временной памяти
+    }
+
+    public void bubbleSorter(){     //МЕТОД ПУЗЫРЬКОВОЙ СОРТИРОВКИ
+        for (int i1 = size(); i1 >= 0; i1--){  //Внешний цикл
+            for (int i2 = 0; i2 < i1; i2++){       //Внутренний цикл
+                if(storage[i2] == null && storage[i1] != null)          //Если порядок элементов нарушен
+                    toSwap(i2, i2 + 1);             //вызвать метод, меняющий местами
+            }
+        }
     }
 }
