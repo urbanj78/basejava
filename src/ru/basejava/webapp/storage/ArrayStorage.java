1aa7b80adfc37e6e4f608fcb17823ddd8d084df7
E.Haritonov <urbanj@yandex.ru>
2023-03-03 08:54:33 +0500

    2 03.03

--- src/ru/basejava/webapp/storage/ArrayStorage.java
+++ src/ru/basejava/webapp/storage/ArrayStorage.java
@@ -9,66 +9,68 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     private int size = 0;
-    final Resume[] STORAGE_LIMIT = new Resume[10000];
+    final int STORAGE_LIMIT = 10000;
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
 
     public void clear() {
-        Arrays.fill(STORAGE_LIMIT, 0, size - 1, null);
+        Arrays.fill(storage, 0, size - 1, null);
         size = 0;
+
     }
 
     public void save(Resume r) {
-        if (size == STORAGE_LIMIT.length) {
-            errMsg(r.getUuid(), 2);
-        } else if (position(r.getUuid()) >= 0) {
-            errMsg(r.getUuid(), 1);
+        if (size == STORAGE_LIMIT) {
+            System.out.println("ERROR  Резюме " + r.getUuid() + " нельзя добавить, хранилище переполнено!");
+        } else if (getIndex(r.getUuid()) >= 0) {
+            System.out.println("ERROR  Резюме " + r.getUuid() + " уже есть в хранилище!");
         } else {
-            STORAGE_LIMIT[size] = r;
+            storage[size] = r;
             size++;
         }
     }
 
     public void update(Resume r) {
-        if (position(r.getUuid()) >= 0) {
+        if (getIndex(r.getUuid()) >= 0) {
             System.out.println("Резюме " + r.getUuid() + " обновлено!");
-        } else errMsg(r.getUuid(), 0);
+        } else {
+            System.out.println("ERROR  Резюме " + r.getUuid() + " нет в хранилище!");
+        }
     }
 
     public Resume get(String uuid) {
-        if (position(uuid) >= 0) {
-            return STORAGE_LIMIT[position(uuid)];
+        int index = getIndex(uuid);
+        if (index >= 0) {
+            return storage[index];
         } else {
-            errMsg(uuid, 0);
+            System.out.println("ERROR  Резюме " + uuid + " нет в хранилище!");
             return null;
         }
     }
 
     public void delete(String uuid) {
-        if (position(uuid) >= 0) {
-            STORAGE_LIMIT[position(uuid)] = STORAGE_LIMIT[size - 1];
-            STORAGE_LIMIT[size - 1] = null;
+        int index = getIndex(uuid);
+        if (index >= 0) {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
             size--;
-        } else errMsg(uuid, 0);
+        }
+        System.out.println("ERROR  Резюме " + uuid + " нет в хранилище!");
     }
 
-    int position(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (STORAGE_LIMIT[i].getUuid().equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 return i;
             }
         }
         return -1;
     }
 
-    void errMsg(String uuid, int err) {
-        String[] msg = new String[]{" нет в хранилище!", " уже есть в хранилище!", " нельзя добавить, хранилище переполнено!"};
-        System.out.println("ERROR  Резюме " + uuid + msg[err]);
-    }
-
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        return Arrays.copyOf(STORAGE_LIMIT, size);
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
